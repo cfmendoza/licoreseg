5ef99b97da74ceea43a5462af6ba8ed1c0aaf7d0
MedicalCenterIPS <fabiansofia1994@gmail.com>
2025-08-03 04:01:28 -0500

    ajuste de rutas

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig(({ mode }) => ({
-    base: mode === 'production' ? '/build/' : '',
+    base: mode === 'production' ? 'https://licoreseg.onrender.com/build/' : '',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
